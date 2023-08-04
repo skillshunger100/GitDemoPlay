1ea2d8dbcdba2905fda55cf520f04a821130e28f
skillshunger100 <2118459@cognizant.com>
2023-08-04 13:41:10 +0530

    commited again

---
+++ src/test/java/com/testing/sample/Debdas1.java
@@ -0,0 +1,13 @@
+package com.testing.sample;
+
+public class Debdas1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Im debdas");
+		System.out.println("I'm a software engineer");
+
+	}
+
+}
